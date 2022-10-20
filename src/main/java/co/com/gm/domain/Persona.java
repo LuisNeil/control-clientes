f7f80b4f4c2eeefa9c6f61f125cd5d328b15598e
Luis Neil <lntejedal@gmail.com>
2022-10-20 14:07:39 -0500

    Validaciones

--- src/main/java/co/com/gm/domain/Persona.java
+++ src/main/java/co/com/gm/domain/Persona.java
@@ -3,6 +3,8 @@ package co.com.gm.domain;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import java.io.Serial;
 import java.io.Serializable;
 
@@ -16,9 +18,17 @@ public class Persona implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotEmpty
     private String nombre;
+
+    @NotEmpty
     private String apellido;
+
+    @NotEmpty
+    @Email
     private String email;
+
     private String telefono;
 
     public Persona(String nombre, String apellido, String email, String telefono) {
